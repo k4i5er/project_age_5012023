83d0de5250306cab01146a19f6b75f692b8a95cc
kaiser <cesar.lpz@gmail.com>
2023-09-14 11:10:34 -0600

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        /*
+            Proyecto:
+            Elaborar un programa que pida la edad al usuario
+            en años, meses y días, así como su fecha de nacimiento
+            en años, meses y días y devuelva su edad en esos mismos
+            términos
+         */
+        }
+    }
+}
