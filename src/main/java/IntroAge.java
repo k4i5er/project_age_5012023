1605303711c255b62298deb1549674db82ede37d
= <=>
2023-09-21 10:32:48 -0600

    Branch get_input_data funcional, @GrizzlyKamen

--- src/main/java/IntroAge.java
+++ src/main/java/IntroAge.java
@@ -4,30 +4,36 @@ public class IntroAge {
         Parametres diaActual = new Parametres();
         Parametres mesActual = new Parametres();
         Parametres yearActual = new Parametres();
-        Parametres validacion = new Parametres();
+        Parametres validacionYourYear = new Parametres();
+        Parametres validacionYourDay = new Parametres();
         Scanner entrada = new Scanner(System.in);
 
-        validacion.valid = true;
+        validacionYourYear.valid = true;
+        validacionYourDay.valid = true;
         diaActual.dias = 18;
         mesActual.meses = 9;
         yearActual.years = 2023;
-        while (!validacion.valid){
+        while (!validacionYourYear.valid){
             System.out.println("Introduce tu año de nacimiento: ");
             int yourYears = entrada.nextInt();
-            if (typeof yourYears == 'string'){
-                validacion.valid = false;
-            }
-            else{
+            if ((Object)yourYears instanceof Integer){
                 System.out.println("Introduce tu mes de nacimiento: ");
                 int yourMonth = entrada.nextInt();
-                if (yourMonth == String){
-                    validacion.valid = false;
-                }
-                else{
+                while (!validacionYourDay.valid){
                     System.out.println("Introduce tu día de nacimiento: ");
                     int yourDay = entrada.nextInt();
-                    if (yourDay)
+                    if ((Object)yourDay instanceof Integer){
+                        System.out.println("A partir de aquí ya es chamba del equipo de Calculate");
+
+                    }
+                    else{
+                        validacionYourDay.valid = false;
+                    }
                 }
+            }
+            else{
+                validacionYourYear.valid = false;
+
             }
 
 
