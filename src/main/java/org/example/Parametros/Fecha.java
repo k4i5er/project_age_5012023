dbe51025f311abe6a569376dd79a3280a418b1e3
= <=>
2023-09-22 12:48:03 -0600

    creacion de lista de meses

---
+++ src/main/java/org/example/Parametros/Fecha.java
@@ -0,0 +1,13 @@
+package org.example.Parametros;
+
+public class Fecha{
+    public static void fecha(String mes){
+            String[] meses={"enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"};
+            for (int i=1; 1=n; i++){
+
+            }
+
+    }
+
+
+}
