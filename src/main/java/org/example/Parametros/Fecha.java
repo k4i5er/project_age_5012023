60b013c00ac1a4df7dd1a33d1c7eec1dc745f0c5
= <18342114@uagro.mx>
2023-09-28 11:23:47 -0600

    Año bisiesto y año no bisiesto

--- src/main/java/org/example/Parametros/Fecha.java
+++ src/main/java/org/example/Parametros/Fecha.java
@@ -1,13 +1,36 @@
 package org.example.Parametros;
 
-public class Fecha{
-    public static void fecha(String mes){
-            String[] meses={"enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"};
-            for (int i=1; 1=n; i++){
+public class Fecha {
+    public static void fecha(String mes) {
+        String[] meses = {"enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
+        for (int i = 1; 1 = n; i++) {
 
-            }
+        }
 
     }
+}
 
+public class AnioBisiesto{
+        public static Anio(String[] anioB){
+            int year=2004;
+
+            int resultado = esBisiesto(year);
+
+            if (resultado ==1) {
+                System.out.println(year + "es un año bisiesto");
+            } else {
+                System.out.prinln(year + "no es un año bisiesto");
+            }
+        }
+
+        public static  int esBisiesto(int year) {
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400==0)){
+                return 1;
+            } else {
+                return 0;
+            }
 
 }
+
+
+
