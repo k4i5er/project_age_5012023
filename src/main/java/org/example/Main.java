867af1c547a6d4a72e9b24b29766eaea8ac168a2
= <18342114@uagro.mx>
2023-10-05 11:49:19 -0600

    Resolviendo conflictos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,36 +1,28 @@
 package org.example;
 import org.example.Parametros.Fecha;
-
 import java.util.Scanner;
-
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         String name;
         String mes;
         Integer numero;
-        Integer  anio;
+        int  anio;
         Integer dia;
         System.out.println("Hola, Bienvenido a mi programa");
         Scanner sc = new Scanner(System.in);
         System.out.print("Escribe el nombre de un mes: ");
         mes = sc.nextLine(); // name = input('Escribe tu nombre')
-        numero=sc.nextInt();
+        //numero=sc.nextInt();
         //Fecha f = new Fecha();
         if (Fecha.nombreDeMes(mes) != -1) {
             System.out.println("El número de mes correspondiente a " + mes + " es " + Fecha.nombreDeMes(mes));
-        } else if (Fecha.numeroDeMes(numero) !=-1){
-            System.out.println("El numero" + numero + " corresponde a "+ Fecha.nombreDeMes(mes) );
-
-
+        } else {
             System.out.println("No se encontró el nombre del mes " + mes);
             System.exit(0);
         }
         System.out.println("Ingresa un año: ");
         anio = sc.nextInt();
-        if (Fecha.esBisiesto(anio == true){
+        if (Fecha.esBisiesto(anio)){
             System.out.println("El año "+anio+" es Bisiesto");
         } else {
             System.out.println("El año " + anio + " NO es bisiesto");
--- src/main/java/org/example/Parametros/Fecha.java
+++ src/main/java/org/example/Parametros/Fecha.java
@@ -1,19 +1,6 @@
 // Fecha.java
 package org.example.Parametros;
 
-public class Fecha {
-    public static void fecha(String mes) {
-        String[] meses = {"enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
-
-        for (int i = 1; i <= meses.length; i++) {
-            if (mes.equals(meses[i - 1])) {
-                System.out.println("El mes ingresado es válido.");
-                return;
-            }
-        }
-        System.out.println("El mes ingresado no es válido.");
-    }
-}
 public class Fecha{
     public Fecha(){
         System.out.println("Está naciendo una nueva instancia...");
@@ -29,23 +16,8 @@ public class Fecha{
 
     }
 
-    public static Integer numeroDeMes(Integer numero){
-        Integer[] numMes={1,2, 3, 4, 5,6, 7, 8, 9, 10, 11, 12};
-        for (int i=0; i<numMes.length; i++){
-            if (numero.equals(numMes[i])){
-                return i+1;
-
-            }
-        }
-        return -1;
-    }
 
     public static Boolean esBisiesto(Integer anio){
-        if(anio % 100 == 0 && anio %4 == 0){
-            return true;
-        } else {
-            return false;
-        }
+        return anio % 100 == 0 && anio % 4 == 0;
     }
 }
-
